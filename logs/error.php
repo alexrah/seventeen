66bceafa9fcd55629c026b8d3cdcaf6d6909cb0e
Ale Sandro <alexrah@gmail.com>
2014-09-08 04:27:58 +0200

    initial commit - jquery.tubular.js fix youtube fullscreen video

---
+++ logs/error.php
@@ -0,0 +1,11 @@
+#
+#<?php die('Forbidden.'); ?>
+#Date: 2014-09-07 23:02:31 UTC
+#Software: Joomla Platform 13.1.0 Stable [ Curiosity ] 24-Apr-2013 00:00 GMT
+
+#Fields: datetime	priority	category	message
+2014-09-07T23:02:31+00:00	INFO	joomlafailure	Username and password do not match or you do not have an account yet.
+2014-09-07T23:02:35+00:00	INFO	joomlafailure	Username and password do not match or you do not have an account yet.
+2014-09-07T23:02:39+00:00	INFO	joomlafailure	Username and password do not match or you do not have an account yet.
+2014-09-07T23:46:42+00:00	INFO	joomlafailure	Username and password do not match or you do not have an account yet.
+2014-09-08T01:03:41+00:00	INFO	joomlafailure	Username and password do not match or you do not have an account yet.
